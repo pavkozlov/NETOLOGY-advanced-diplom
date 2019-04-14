386215b2d6b60dc30a1f4b295e63d8b4eccb1482
Pavel Kozlov <it.pavelkozlov@gmail.com>
2019-04-14 11:14:35 +0300

    v0.2 add auth with access_token

--- VKinder.py
+++ VKinder.py
@@ -1,18 +1,52 @@
 import vk_api
+from urllib.parse import urlencode
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def get_access_token(my_login, my_password):
+    app_id = 6854739
+    auth_url = 'https://oauth.vk.com/authorize'
+    auth_data = {
+        'client_id': app_id,
+        'display': 'page',
+        'scope': 'status, friends',
+        'response_type': 'token',
+        'v': '5.95',
+        'redirect_uri': ''
+    }
+    url = '?'.join((auth_url, urlencode(auth_data)))
+
+    browser = webdriver.Firefox()
+    browser.get(url)
+    login = browser.find_element_by_name('email')
+    password = browser.find_element_by_name('pass')
+    login.send_keys(my_login)
+    password.send_keys(my_password)
+    password.send_keys(Keys.ENTER)
+    time.sleep(1)
+    url = browser.current_url
+    browser.quit()
+    pattern = 'access_token=(.*)&expires_in'
+    re.compile(pattern)
+    res = re.search(pattern, url).group(1)
+    return res
+
+
+access_token = get_access_token('login', 'password')
 
 
 class VKinder:
-    def __init__(self, login, password, **kwargs):
-        self.login = login
-        self.password = password
-        self.vk = self.authorize_in_vk(self.login, self.password)
+    def __init__(self, **kwargs):
+        self.vk = self.authorize_in_vk()
         self.find_gender = self.get_own_gender()
         self.city = self.get_own_city()
         self.kwargs = kwargs
 
-    def authorize_in_vk(self, login, password):
-        vk_session = vk_api.VkApi(login, password)
-        vk_session.auth()
+    def authorize_in_vk(self):
+        vk_session = vk_api.VkApi(token=access_token)
         vk = vk_session.get_api()
         return vk
 
@@ -44,5 +78,6 @@ class VKinder:
         return result
 
 
-pavel_kozlov = VKinder('+79660684435', '', sex=1)
-print(pavel_kozlov.get_users_with_books_and_interests())
+if __name__ == '__main__':
+    pavel_kozlov = VKinder(sex=2)
+    print(pavel_kozlov.get_users_with_books_and_interests())
