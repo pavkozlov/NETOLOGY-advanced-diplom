74c035dea964a7505a1180a70750337b86559147
pkozlov <it.pavelkozlov@gmail.com>
2019-04-18 15:56:44 +0300

    reformat code

--- VKinder.py
+++ VKinder.py
@@ -1,191 +1,192 @@
 import datetime
 import vk_api
 from pymongo import MongoClient
-import get_token_from_login
 import re
 
-client = MongoClient()
-vkinder_db = client.vkinder_db
-
-# INPUT YOUR TOKEN HERE!!!
-TOKEN = get_token_from_login.get_access_token(get_token_from_login.LOGIN, get_token_from_login.PASSWORD)
+TOKEN = '7aea4bcb72aa437b3b5c739a6acf43deeab50869788cd4579d4cacd5aa83ec2f3e000297025eba5ff9e01'
 
 
-class VKinder:
-    def __init__(self, age_from, age_to, **kwargs):
+class VKinderDatabase:
+    def __init__(self):
+        self.client = MongoClient()
+        self.vkinder_db = self.client.vkinder_db
+
+    def insert_many_people(self, my_list):
+        if self.vkinder_db.all_people.count_documents({}) == 0:
+            self.vkinder_db.all_people.insert_many(my_list)
+
+    def find_all_people(self):
+        result = self.vkinder_db.all_people.find({})
+        return result
+
+    def insert_one(self, my_dict):
+        if self.vkinder_db.own_account.count_documents({}) == 0:
+            self.vkinder_db.own_account.insert_one(my_dict)
+
+    def find_one(self):
+        result = self.vkinder_db.own_account.find({})[0]
+        return result
+
+    def insert_many_partners(self, my_list):
+        if self.vkinder_db.partners.count_documents({}) == 0:
+            self.vkinder_db.partners.insert_many(my_list)
+
+    def find_many_partners(self):
+        result = self.vkinder_db.partners.find({})
+        return result
+
+
+class VKinderVK:
+    def __init__(self, age_from=18, age_to=18, **kwargs):
+        self.vk = self.authorize_by_token()
         self.age_from = age_from
         self.age_to = age_to
-        self.kwargs = kwargs
 
-        self.vk = self.authorize_in_vk_by_token()
+        if kwargs.get('sex'):
+            self.sex = kwargs['sex']
+        else:
+            self.sex = self.get_sex()
 
-        self.find_gender = self.get_own_gender()
-        self.city = self.get_own_city()
+        if kwargs.get('city'):
+            self.city = kwargs['city']
+        else:
+            self.city = self.get_city()
 
-        self.write_into_db()
-        self.get_own_books_and_interests()
-
-    def authorize_in_vk_by_token(self):
-        access_token = TOKEN
-        vk_session = vk_api.VkApi(token=access_token)
+    def authorize_by_token(self):
+        vk_session = vk_api.VkApi(token=TOKEN)
         vk = vk_session.get_api()
         return vk
 
-    def get_users(self):
-        if self.kwargs.get('sex') == None:
-            self.kwargs['sex'] = self.find_gender
-
-        if self.kwargs.get('city') == None:
-            self.kwargs['city'] = self.city
-
-        result = list()
-        for i in range(self.age_from, self.age_to + 1):
-            search = self.vk.users.search(count=1000, fields='books,interests,music', age_from=i, age_to=i, **self.kwargs)
-            result.append(search['items'])
-        return result
-
-    def write_into_db(self):
-        if vkinder_db.all_people.count_documents({}) == 0:
-            all_users = self.get_users()
-            formated_list = list()
-            pattern = ',\s?|\.\s?,?'
-            pattern = re.compile(pattern)
-
-            for peoples in all_users:
-                for people in peoples:
-                    user = dict()
-
-                    user['first_name'] = people['first_name']
-                    user['last_name'] = people['last_name']
-
-                    try:
-                        user_books = people['books']
-                        if len(user_books) > 0:
-                            user['books'] = re.split(pattern, user_books)
-                        else:
-                            raise KeyError
-                    except KeyError:
-                        user['books'] = list()
-
-                    try:
-                        user_music = people['music']
-                        if len(user_music) > 0:
-                            user['music'] = re.split(pattern, user_music)
-                        else:
-                            raise KeyError
-                    except KeyError:
-                        user['music'] = list()
-
-                    try:
-                        user_interests = people['interests']
-                        if len(user_interests) > 0:
-                            user['interests'] = re.split(pattern, user_interests)
-                        else:
-                            raise KeyError
-                    except KeyError:
-                        user['interests'] = list()
-
-                    try:
-                        user_groups = self.find_groups(people['id'])
-                        user['groups'] = user_groups
-                    except vk_api.exceptions.ApiError:
-                        user['groups'] = list()
-
-                    formated_list.append(user)
-
-            vkinder_db.all_people.insert_many(formated_list)
-
-    def get_own_city(self):
+    def get_city(self):
         city = self.vk.account.getProfileInfo()['city']['id']
         return city
 
-    def get_own_gender(self):
-        gender = self.vk.account.getProfileInfo()['sex']
-        if gender == 1:
-            return 2
-        elif gender == 2:
-            return 1
+    def get_sex(self):
+        sex = self.vk.account.getProfileInfo()['sex']
+        return 2 if sex == 1 else 1
 
-    def find_groups(self, user_id=None):
-        res = self.vk.groups.get(user_id=user_id)['items']
-        return res
+    def search_users(self, count=10):
+        result = list()
+        for i in range(self.age_from, self.age_to + 1):
+            search = self.vk.users.search(count=count, fields='books,interests,music', age_from=i, age_to=i,
+                                          city=self.city, sex=self.sex)
+            result += search['items']
+        return result
 
-    def get_own_books_and_interests(self):
+    def get_groups(self, user_id=None):
+        result = self.vk.groups.get(user_id=user_id)['items']
+        return result
 
-        if vkinder_db.own_account.count_documents({}) == 0:
+    def get_my_profile(self):
+        result = self.vk.users.get(fields='books,interests,music')[0]
+        if len(result['books']) == 0:
+            result['books'] = input("Введите ваши предпочтения в книгах: ")
+        if len(result['music']) == 0:
+            result['music'] = input("Введите ваши предпочтения в музыке: ")
+        if len(result['interests']) == 0:
+            result['interests'] = input("Введите ваши интересы: ")
+        return result
 
-            result = self.vk.users.get(fields='books,interests,music')
-            pattern = '[.,;]'
-            pattern = re.compile(pattern)
-            my_books = result[0]['books'].split()
 
-            if len(my_books) == 0:
-                my_books = input("Введите ваши предпочтения в книгах: ")
-                my_books = re.split(pattern, my_books)
+class VKinderData:
+    def format_users(self, all_users):
+        result = list()
+        for people in all_users:
+            user = dict()
+            user['first_name'] = people['first_name']
+            user['last_name'] = people['last_name']
 
-            my_music = result[0]['music']
-            if len(my_music) == 0:
-                my_music = input("Введите ваши предпочтения в музыке: ")
-                my_music = re.split(pattern, my_music)
+            if people.get('books'):
+                user['books'] = people['books']
+            else:
+                user['books'] = ''
+            if people.get('music'):
+                user['music'] = people['music']
+            else:
+                user['music'] = ''
+            if people.get('interests'):
+                user['interests'] = people['interests']
+            else:
+                user['interests'] = ''
 
-            my_interests = result[0]['interests']
-            if len(my_interests) == 0:
-                my_interests = input("Введите ваши интересы: ")
-                my_interests = re.split(pattern, my_interests)
+            try:
+                user_groups = VKinderVK.get_groups(self=VKinderVK(), user_id=people['id'])
+                user['groups'] = user_groups
+            except vk_api.exceptions.ApiError:
+                user['groups'] = list()
 
-            vkinder_db.own_account.insert_one({
-                'first_name': result[0]['first_name'],
-                'last_name': result[0]['last_name'],
-                'books': my_books,
-                'music': my_music,
-                'interests': my_interests,
-                'id': result[0]['id'],
-                'added_time': datetime.datetime.now(),
-                'groups': self.find_groups()
-            })
+            user['added_time'] = datetime.datetime.now()
+            result.append(user)
+        return result
 
-    def find_ideal(self):
-        ideal_for_me = list()
-        me = vkinder_db.own_account.find({})
-        partners = vkinder_db.all_people.find({})
+    def format_my_profile(self, my_profile):
+        result = {
+            'first_name': my_profile['first_name'],
+            'last_name': my_profile['last_name'],
+            'books': my_profile['books'],
+            'music': my_profile['music'],
+            'interests': my_profile['interests'],
+            'groups': VKinderVK.get_groups(self=VKinderVK()),
+            'added_time': datetime.datetime.now()
+        }
+        return result
 
-        my_books = me[0]['books']
-        re_books = "(" + ")|(".join(my_books) + ")"
-        re_books = re.compile(re_books)
 
-        my_music = me[0]['music']
-        re_music = "(" + ")|(".join(my_music) + ")"
-        re_music = re.compile(re_music)
+class VKinderSearch:
+    def find_ideal(self, my_own_profile, all_users_list):
+        result = list()
 
-        my_interests = me[0]['interests']
-        re_interests = "(" + ")|(".join(my_interests) + ")"
-        re_interests = re.compile(re_interests)
+        my_profile = my_own_profile
+        all_users = all_users_list
 
-        my_groups = me[0]['groups']
+        my_books = re.split('[,.\s]', my_profile['books'])
+        my_books = '.*?(' + ')?('.join(my_books) + ').*?'
+        my_books = re.compile(my_books, re.IGNORECASE)
 
-        for i in partners:
+        my_music = re.split('[,.\s]', my_profile['music'])
+        my_music = '.*?(' + ')?('.join(my_music) + ').*?'
+        my_music = re.compile(my_music, re.IGNORECASE)
 
-            both_in_group = list(set(i['groups']).intersection(my_groups))
-            if len(both_in_group) > 0 and i not in ideal_for_me:
-                ideal_for_me.append(i)
+        my_interests = re.split('[,.\s]', my_profile['interests'])
+        my_interests = '.*?(' + ')?('.join(my_interests) + ').*?'
+        my_interests = re.compile(my_interests, re.IGNORECASE)
 
-            same_books = list(filter(re_books.search, i['books']))
-            if len(same_books) > 0 and i not in ideal_for_me:
-                ideal_for_me.append(i)
+        my_groups = my_profile['groups']
 
-            same_music = list(filter(re_music.search, i['music']))
-            if len(same_music) > 0 and i not in ideal_for_me:
-                ideal_for_me.append(i)
+        for i in all_users:
+            res = my_books.search(i['books'])
+            if res and i not in result:
+                result.append(i)
+            res = my_music.search(i['music'])
+            if res and i not in result:
+                result.append(i)
+            res = my_interests.search(i['interests'])
+            if res and i not in result:
+                result.append(i)
+            if len(list(set(my_groups).intersection(i['groups']))) > 0 and i not in result:
+                result.append(i)
 
-            same_interests = list(filter(re_interests.search, i['interests']))
-            if len(same_interests) > 0 and i not in ideal_for_me:
-                ideal_for_me.append(i)
+        return result
 
-            del i
 
-        vkinder_db.ideal_partner.insert_many(ideal_for_me)
+def run():
+    vk = VKinderVK(age_from=18, age_to=200)
+    users = vk.search_users(count=100)
+    my_account = vk.get_my_profile()
+
+    vk_data = VKinderData()
+    users = vk_data.format_users(users)
+    my_account = vk_data.format_my_profile(my_account)
+
+    vk_db = VKinderDatabase()
+    vk_db.insert_many_people(users)
+    vk_db.insert_one(my_account)
+
+    vk_search = VKinderSearch()
+    reccomended = vk_search.find_ideal(my_account, users)
+
+    vk_db.insert_many_partners(reccomended)
 
 
 if __name__ == '__main__':
-    pavel_kozlov = VKinder(token=False, age_from=18, age_to=20)
-    pavel_kozlov.find_ideal()
+    run()
